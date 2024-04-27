fc3cf5452f8fe6c96f6ad0ca433f8cbefd8d7365
Fc24prog <166566894+Fc24prog@users.noreply.github.com>
2024-04-26 23:23:05 -0300

    Add files via upload
    #Faça um programa que leia 2 valores: X e Y. A seguir, calcule e mostre a soma dos números
    #pares entre eles.

---
+++ 11.py
@@ -0,0 +1,18 @@
+
+x=int(input("Entre com o valor de x: "))
+y=int(input("Entre com o valor de y: "))
+
+par=0
+
+for i in range (x,y):
+
+    if (i%2==0):
+
+        par=par+i
+
+    
+        print(par)
+
+#Faça um programa que leia 2 valores: X e Y. A seguir, calcule e mostre a soma dos números
+#pares entre eles.
+
