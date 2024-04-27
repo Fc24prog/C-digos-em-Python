8b02d350d101189cfa675c63f7a891cfe9e8dc4e
Fc24prog <166566894+Fc24prog@users.noreply.github.com>
2024-04-26 23:19:53 -0300

    Add files via upload
    #Faça um programa que imprima, ao final, a soma da série a seguir. Obs.: Será solicitado ao
    #usuário apenas o valor de n, o a valor de m deve ser deduzido pela própria série.
    #S = 1/1 + 2/3 + 3/5 + 4/7 + 5/9 + ... + n/m

---
+++ 7.py
@@ -0,0 +1,12 @@
+soma= 0
+n=int(input("Entre com o valor de n: "))
+
+for i in range(1,n+1):
+    soma=soma+(i/2*i-1)
+    
+print (soma)
+
+#Faça um programa que imprima, ao final, a soma da série a seguir. Obs.: Será solicitado ao
+#usuário apenas o valor de n, o a valor de m deve ser deduzido pela própria série.
+#S = 1/1 + 2/3 + 3/5 + 4/7 + 5/9 + ... + n/m
+
