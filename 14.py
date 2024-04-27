966951ff2466fb4ecddbe65a848c282b65d495e4
Fc24prog <166566894+Fc24prog@users.noreply.github.com>
2024-04-26 23:27:41 -0300

    Add files via upload

---
+++ 14.py
@@ -0,0 +1,13 @@
+b=int(input("Entre com o valor da base: "))
+h=int(input("Entre com o valor da altura: "))
+
+
+while ( b>0 and h>0):
+
+    a=(b*h)/2
+        
+
+    print ("a area é: ",a)
+
+    b=int(input("Entre com o valor da base: "))
+    h=int(input("Entre com o valor da altura: "))
