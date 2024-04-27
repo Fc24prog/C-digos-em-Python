94e4bab270f1e1f8d8a115c20beb9b6a7efaead9
Fc24prog <166566894+Fc24prog@users.noreply.github.com>
2024-04-26 23:20:56 -0300

    Add files via upload
    #Faça um programa que leia 2 valores: X e Y. A seguir, calcule e exiba a soma dos números
    #ímpares entre eles.

---
+++ 10.py
@@ -0,0 +1,34 @@
+
+x=int(input("Entre com o valor de x: "))
+y=int(input("Entre com o valor de y: "))
+
+imp=0
+
+for i in range (x,y):
+
+    if (i%2==1):
+
+        imp=imp+i
+
+    
+        print(imp)
+
+
+
+
+
+
+
+
+
+
+
+
+#Faça um programa que leia 2 valores: X e Y. A seguir, calcule e exiba a soma dos números
+#ímpares entre eles.
+
+
+
+
+
+
