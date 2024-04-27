8f3e2dc43e44c74eb55ac7954c3f0dd85f07d4c6
Fc24prog <166566894+Fc24prog@users.noreply.github.com>
2024-04-26 23:12:42 -0300

    Add files via upload
    #3. Faça um programa que peça 10 números inteiros, calcule e mostre a quantidade de números pares
    #e a quantidade de números impares.

---
+++ 3.py
@@ -0,0 +1,18 @@
+contp=0
+conti=0
+
+for i in range (0,8):
+
+    num= int(input("Entre com os numeros: "))
+    
+    if(num%2==0):
+        contp=contp+1
+        
+    else:
+        conti=conti+1
+        
+print("A quantidade de numeros pares sao: ",contp)
+print("A quantidade de numeros impares sao: ",conti)
+
+#3. Faça um programa que peça 10 números inteiros, calcule e mostre a quantidade de números pares
+#e a quantidade de números impares.
